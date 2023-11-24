d1a0dc65a5eeebf37e1fe7426a03cf742d2ffbff
Luis Manuel Lepe <luismanuel.lepe@gmail.com>
2023-11-23 19:03:08 -0600

    Modificado monitor CPU, agregado LED

--- monitorCpu.py
+++ monitorCpu.py
@@ -1,3 +1,14 @@
+from gpiozero import LED
+from time import sleep
 import psutil
 
-print(psutil.cpu_percent())
+led = LED(14)
+
+while True:
+    cpu = psutil.cpu_percent()
+    print(cpu)
+    if (cpu > 30):
+        led.on()
+    else:
+        led.off()
+    sleep(.2)
